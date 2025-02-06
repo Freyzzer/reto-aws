cabd6c47ad40ae466cc985aba6046f21b82a7756
Freyzzer George Valencia Murillo <freyzzergvm12@gmail.com>
2025-02-06 10:37:09 -0500

    Intento 6 para desplegar a s3

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          hola 
+          hola dadsdsdsdasdsadas
         </p>
       </div>
       <p className="read-the-docs">
